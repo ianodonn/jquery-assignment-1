48651af204149cdc3980ff082c0d2d239ec64e9d
Ian O'Donnell <iano@id.iit.edu>
2017-04-27 18:19:34 -0500

    added background, sun, grass

--- part-1/script.js
+++ part-1/script.js
@@ -1,1 +1,10 @@
 // Do your work in this file.
+
+$('<div>',{'class':'view'})
+.appendTo(document.body);
+
+$('<div>',{'class':'grass'})
+.appendTo('.view');
+
+$('<div>',{'class':'sun'})
+.appendTo('.view');
