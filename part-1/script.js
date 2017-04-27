11098345293da661170e9ccc3c69156cb2b82e84
Ian O'Donnell <iano@id.iit.edu>
2017-04-27 18:23:56 -0500

    added rays to the sun

--- part-1/script.js
+++ part-1/script.js
@@ -1,10 +1,18 @@
 // Do your work in this file.
 
-$('<div>',{'class':'view'})
-.appendTo(document.body);
+$('<div>', { 'class': 'view' })
+    .appendTo(document.body);
 
-$('<div>',{'class':'grass'})
-.appendTo('.view');
+$('<div>', { 'class': 'grass' })
+    .appendTo('.view');
 
-$('<div>',{'class':'sun'})
-.appendTo('.view');
+$('<div>', { 'class': 'sun' })
+    .appendTo('.view');
+
+var i = 1;
+
+while (i < 6) {
+    $('<div>', { 'class': 'ray-' + i })
+        .appendTo('.sun');
+        i++;
+}
