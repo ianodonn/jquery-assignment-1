a74efe98a98dcacbc6145eaa10f755b98f423050
Ian O'Donnell <iano@id.iit.edu>
2017-04-27 19:42:22 -0500

    accidentally wrote all of the code without committing, whoops

--- part-2/script.js
+++ part-2/script.js
@@ -1,1 +1,31 @@
 // Do your work in this file.
+
+// $('<div>', { 'class': 'box' })
+//     .appendTo(document.body);
+
+for (var i = 1; i < 197; i++) {
+    $('<div>', { 'class': 'box' })
+    .appendTo(document.body);
+
+    // if (i % 2 == 0) {
+    //     $('.box').addClass('type-1');
+    //     console.log("i is even");
+    // } else {
+    //     $('.box').addClass('type-2');
+    //     console.log("i is odd");
+    // }
+    $('div').addClass(function(index, currentClass){
+        var newClass;
+        if(index % 2==0){
+            newClass = "type-1";
+        }else{
+            newClass = "type-2";
+        }
+        return newClass;
+    });
+
+}
+function boxClicked(){
+    window.alert('hey');
+}
+$('.box').click(boxClicked);
